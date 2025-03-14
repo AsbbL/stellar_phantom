2b8b73cac4b6bf8e2946be0c43563d67bc2876dc
mbbsa <asbbmult168@gmail.com>
2025-03-14 11:41:28 +0000

    create virtualnetwork

--- bbsafolder/main.tf
+++ bbsafolder/main.tf
@@ -6,17 +6,17 @@ resource "azurerm_resource_group" "examplebbsa" {
 }
 
 # Virtual Network
-resource "azurerm_virtual_network" "bbsafolder-VN" {
-  name                = "bbsafolder-vnet"
+resource "azurerm_virtual_network" "bbsavnet" {
+  name                = "bbsavnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.examplebbsa.location
   resource_group_name = azurerm_resource_group.examplebbsa.name
 }
 
 # Subnet 1
-resource "azurerm_subnet" "bbsasubnet1" {
-  name                 = "bbsasubnet-1"
+resource "azurerm_subnet" "bbsasubnet" {
+  name                 = "bbsasubnet1"
   resource_group_name  = azurerm_resource_group.examplebbsa.name
-  virtual_network_name = azurerm_virtual_network.bbsa_terraform_network.name
+  virtual_network_name = azurerm_virtual_network.bbsavnet.name
   address_prefixes     = ["10.0.0.0/24"]
 }
