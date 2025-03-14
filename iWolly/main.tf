e706da67fa264f2ded2acacf7f5db3116ce53c05
iWolly <75027377+iWolly@users.noreply.github.com>
2025-03-14 11:07:43 +0000

    create vnet and subnet

--- iWolly/main.tf
+++ iWolly/main.tf
@@ -8,3 +8,18 @@ tags = {
   }
 }
 
+# Virtual Network
+resource "azurerm_virtual_network" "iWolly-vnet" {
+  name                = "iWolly-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.iWolly_RG.location
+  resource_group_name = azurerm_resource_group.iWolly_RG.name
+}
+
+# Subnet 1
+resource "azurerm_subnet" "iWolly-subnet-1" {
+  name                 = "iWolly-subnet-1"
+  resource_group_name  = azurerm_resource_group.iWolly_RG.name
+  virtual_network_name = azurerm_virtual_network.iWolly-vnet.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
