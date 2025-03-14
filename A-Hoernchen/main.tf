77087d25fbfc12fd7ef69921bab5c38e7e4bb9aa
A-Hoernchen <159808687+Alpha-Hoernchen@users.noreply.github.com>
2025-03-14 08:41:11 +0000

    new try

---
+++ A-Hoernchen/main.tf
@@ -0,0 +1,14 @@
+# Create a resource group
+resource "azurerm_resource_group" "Hoernchen_RG" {
+  name     = "Hoernchen-resources"
+  location = "West Europe"
+}
+
+# Create a virtual network within the resource group
+# resource "azurerm_virtual_network" "Hoernchen_Net" {
+#  name                = "Hoernchen-network"
+#  resource_group_name = azurerm_resource_group.Hoernchen_RG.name
+#  location            = azurerm_resource_group.Hoernchen_RG.location
+#  address_space       = ["10.1.0.0/16"]
+#}
+
